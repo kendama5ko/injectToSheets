a95f380bcb6dd1c98dc6183c4457e03fb55f0f8e
kendama5ko <hukurahagix@yahoo.co.jp>
2024-10-22 09:19:46 +0900

    [抽象メソッド] extractDataの追加

--- src/main/java/org/example/Extractor/DataExtractor.java
+++ src/main/java/org/example/Extractor/DataExtractor.java
@@ -1,4 +1,14 @@
 package org.example.Extractor;
 
 public interface DataExtractor {
+    /**
+     * 与えられた文字列 `text` から正規表現に一致する部分を見つけ出し、
+     * それを `String[]` 配列として返します。
+     * 配列の各要素は、一致したデータを順番に格納します。
+     *
+     * @param text 対象となるテキスト
+     * @return 正規表現に一致したデータの配列。
+     *         一致するデータがない場合は、空の配列を返します。
+     */
+    public String[] extractData(String text);
 }
