ba0c1dfa59614fcd346303fa494dc868e07c2d56
kendama5ko <hukurahagix@yahoo.co.jp>
2024-10-24 10:36:21 +0900

    [クラス] データの抽出と書き込みを指示するTextProcessorクラスの追加

--- src/main/java/org/example/TextProcessor.java
+++ src/main/java/org/example/TextProcessor.java
@@ -1,4 +1,19 @@
 package org.example;
 
+import org.example.Extractor.DataExtractor;
+import org.example.googleapi.writer.DataWriter;
+
 public class TextProcessor {
+
+    /**
+     * RegexExtractor.extractDataメソッドからデータを抽出し、SheetsWriter.writeメソッドでデータを書き込む
+     *
+     * @param text オリジナルのテキストデータ
+     * @param extractor text からregexで指定されたデータを抽出するためのクラスのインスタンス
+     * @param writer 抽出されたデータを書き込むためのクラスのインスタンス
+     */
+    public void processText(String text, DataExtractor extractor, DataWriter writer) {
+        String[] extractedData = extractor.extractData(text);
+        writer.write(extractedData);
+    }
 }
