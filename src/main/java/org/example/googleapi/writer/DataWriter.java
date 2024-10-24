519f6790dd1cda7b77440aa24cacbdd716ff44db
kendama5ko <hukurahagix@yahoo.co.jp>
2024-10-24 09:56:16 +0900

    [writer pkg] DataWriter, SheetsWriter クラスの追加 - write(String[] data)

--- src/main/java/org/example/googleapi/writer/DataWriter.java
+++ src/main/java/org/example/googleapi/writer/DataWriter.java
@@ -1,4 +1,11 @@
 package org.example.googleapi.writer;
 
 public interface DataWriter {
+
+    /**
+     * Google Sheets APIからスプレッドシートに 'data' を書き込む
+     *
+     * @param data RegexExtractor.extractDataメソッドで抽出したデータ
+     */
+    void write(String[] data);
 }
--- src/main/java/org/example/googleapi/writer/SheetsWriter.java
+++ src/main/java/org/example/googleapi/writer/SheetsWriter.java
@@ -1,4 +1,52 @@
 package org.example.googleapi.writer;
 
-public class SheetsWriter {
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import org.example.googleapi.service.SheetsService;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SheetsWriter implements DataWriter{
+
+    private final SheetsService sheetsService;
+    private static final String APPLICATION_NAME = "InjectToSheets";
+    private static final String FILEID =
+            "1mVZ_s2KjmEATftnagsp33w7UUpigiwDc42d1lA2IxMM";
+    private static final String RANGE = "精密採点!D4:O4";
+    private static final int SHEETID = 76597951;
+
+    public SheetsWriter(SheetsService sheetsService) {
+        this.sheetsService = sheetsService;
+    }
+
+    /**
+     * Google Sheets APIからスプレッドシートに 'data' を書き込む
+     *
+     * @param data RegexExtractor.extractDataメソッドで抽出したデータ
+     */
+    @Override
+    public void write(String[] data) {
+        try {
+            Sheets sheets = sheetsService.createSheets();
+
+            // 配列の各要素をセルにセットする
+            ValueRange injectData =
+                    new ValueRange().setValues(Collections.singletonList(Arrays.asList(data)));
+
+            // Sheetsからスプレッドシートに書き込む
+            sheets.spreadsheets().values()
+                    .update(FILEID, RANGE, injectData)
+                    .setValueInputOption("RAW") // データの形式はRAWもしくはUSER_ENTERED
+                    .execute();
+
+
+        } catch (IOException | GeneralSecurityException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
